04c8703d58b4cae855df0cd2ba03fd3afa354eed
BLooDBRothER <computertechnology446@gmail.com>
2022-05-26 06:03:58 +0530

    create register form and fix responsive

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,86 +1,97 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-container');
-let formBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-container');
-let formClose = document.querySelector('#form-close');
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-let imgBtn = document.querySelectorAll('.img-btn');
+const searchBtn = document.querySelector('#search-btn');
+const searchBar = document.querySelector('.search-bar-container');
+const formBtn = document.querySelector('#login-btn');
+const loginForm = document.querySelector('.login-form-container');
+const formClose = document.querySelector('#form-close');
+const menu = document.querySelector('#menu-bar');
+const navbar = document.querySelector('.navbar');
+const imgBtn = document.querySelectorAll('.img-btn');
+const formChangeBtn = document.querySelectorAll(".change-link");
 
-window.onscroll = () =>{
-    searchBtn.classList.remove('fa-times');
-    searchBar.classList.remove('active');
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-    loginForm.classList.remove('active');
+window.onscroll = () => {
+  searchBtn.classList.remove('fa-times');
+  searchBar.classList.remove('active');
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  loginForm.classList.remove('active');
 }
 
-menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menu.addEventListener('click', () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
 });
 
-searchBtn.addEventListener('click', () =>{
-    searchBtn.classList.toggle('fa-times');
-    searchBar.classList.toggle('active');
+searchBtn.addEventListener('click', () => {
+  searchBtn.classList.toggle('fa-times');
+  searchBar.classList.toggle('active');
 });
 
-formBtn.addEventListener('click', () =>{
-    loginForm.classList.add('active');
+formBtn.addEventListener('click', () => {
+  loginForm.classList.add('active');
 });
 
-formClose.addEventListener('click', () =>{
-    loginForm.classList.remove('active');
+formClose.addEventListener('click', () => {
+  loginForm.classList.remove('active');
 });
 
-imgBtn.forEach(btn =>{
-    btn.addEventListener('click', ()=>{
-        document.querySelector('.controls .active').classList.remove('active');
-        btn.classList.add('active');
-        let src = btn.getAttribute('data-src');
-        document.querySelector('#video-slider').src = src;
-    });
+imgBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.controls .active').classList.remove('active');
+    btn.classList.add('active');
+    let src = btn.getAttribute('data-src');
+    document.querySelector('#video-slider').src = src;
+  });
+});
+
+formChangeBtn.forEach(elem => {
+  elem.addEventListener("click", (e) => {
+    console.log(e.target.dataset.value);
+    const toActivate = document.querySelector("form.none");
+    const toDeactivate = document.querySelector(`form.${e.target.dataset.value}`);
+    toActivate.classList.remove("none");
+    toDeactivate.classList.add("none");
+  });
 });
 
 var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
     },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
+    768: {
+      slidesPerView: 2,
     },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
 
 var swiper = new Swiper(".brand-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        450: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1200: {
-          slidesPerView: 5,
-        },
-      },
+    768: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
 });
