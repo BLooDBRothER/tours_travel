689da002eeaa15d3b614f521a72e3368e3a6e643
BLooDBRothER <computertechnology446@gmail.com>
2022-05-26 06:43:14 +0530

    Create package booking form

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,19 +1,22 @@
 const searchBtn = document.querySelector('#search-btn');
 const searchBar = document.querySelector('.search-bar-container');
 const formBtn = document.querySelector('#login-btn');
-const loginForm = document.querySelector('.login-form-container');
-const formClose = document.querySelector('#form-close');
+const floatForm = document.querySelectorAll('.float-form-container');
+const formClose = document.querySelectorAll('#form-close');
 const menu = document.querySelector('#menu-bar');
 const navbar = document.querySelector('.navbar');
 const imgBtn = document.querySelectorAll('.img-btn');
 const formChangeBtn = document.querySelectorAll(".change-link");
+const bookPackageBtn = document.querySelectorAll(".book-btn")
 
 window.onscroll = () => {
   searchBtn.classList.remove('fa-times');
   searchBar.classList.remove('active');
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
-  loginForm.classList.remove('active');
+  floatForm.forEach(form => {
+    form.classList.remove('active')
+  });
 }
 
 menu.addEventListener('click', () => {
@@ -27,12 +30,14 @@ searchBtn.addEventListener('click', () => {
 });
 
 formBtn.addEventListener('click', () => {
-  loginForm.classList.add('active');
+  floatForm[0].classList.add('active');
 });
 
-formClose.addEventListener('click', () => {
-  loginForm.classList.remove('active');
-});
+formClose.forEach(closeBtn => {
+  closeBtn.addEventListener("click", (e) => {
+    floatForm[+e.target.dataset.index].classList.remove('active');
+  });
+})
 
 imgBtn.forEach(btn => {
   btn.addEventListener('click', () => {
@@ -45,7 +50,6 @@ imgBtn.forEach(btn => {
 
 formChangeBtn.forEach(elem => {
   elem.addEventListener("click", (e) => {
-    console.log(e.target.dataset.value);
     const toActivate = document.querySelector("form.none");
     const toDeactivate = document.querySelector(`form.${e.target.dataset.value}`);
     toActivate.classList.remove("none");
@@ -53,6 +57,20 @@ formChangeBtn.forEach(elem => {
   });
 });
 
+bookPackageBtn.forEach(bookBtn => {
+  bookBtn.addEventListener("click", (e) => {
+    floatForm[1].classList.add("active");
+    updateBookForm(e.target.dataset);
+  });
+});
+
+function updateBookForm(packageData) {
+  floatForm[1].querySelector(".pkg-name").value = packageData.name;
+  floatForm[1].querySelector(".pkg-price").value = packageData.price;
+  floatForm[1].querySelector(".btn").dataset.id = packageData.id;
+}
+
+// Swiper function
 var swiper = new Swiper(".review-slider", {
   spaceBetween: 20,
   loop: true,
