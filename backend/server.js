53f2b536fee9f80391c911727c12ad957ec4f03e
Jayashakthi28 <vishnu28.js@gmail.com>
2022-05-28 17:58:16 +0530

    Add booking route to server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app=express();
 const userRoutes=require('./routes/user');
 const packageRoutes = require("./routes/package");
 const imageRoutes=require("./routes/image");
+const bookingRoutes=require("./routes/booking");
 const path=require('path');
 const cors=require('cors');
 
@@ -16,6 +17,7 @@ app.use('/photos',express.static(path.join(__dirname,"./uploads")));
 app.use('/user',userRoutes);
 app.use('/package',packageRoutes);
 app.use('/image',imageRoutes);
+app.use('/booking',bookingRoutes);
 //server
 app.listen(3000,()=>{
     console.log("Server running at port 3000");
